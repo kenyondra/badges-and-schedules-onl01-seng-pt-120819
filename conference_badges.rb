d3f607114e33b32139c6c997f8030530b2cc3777
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-02-23 02:55:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end
+  def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
 
-def batch_badge_creator(attendees)
- attendees.collect do |name|
-   badge_maker(name)
+  def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
 end
 
 
