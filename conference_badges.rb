cf5847043dcfc1d6498674b5fa9efe754f635bc3
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-02-23 03:05:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,10 @@ def assign_rooms(attendees)
   new_array = []
   attendees.each_with_index do |person, index|
     new_array << "Hello, #{person}! You'll be assigned to room #{index + 1}!"
-    
+  end
+  
+  new_array
+end
   
 
 
